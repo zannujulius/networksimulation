b997e11247346744232ece6b511276395dc33e0d
zannujulius <zannujulius14@gmail.com>
2023-01-03 16:57:53 +0100

    fixed timer

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ setInterval(() => {
 
 function update(element, distance) {
   var width = 1;
-  var identity = setInterval(scene, 200);
+  var identity = setInterval(scene, 90);
   function scene() {
     if (width >= distance) {
       clearInterval(identity);
@@ -47,8 +47,43 @@ function two1() {
   update(rf, 85);
 }
 
+// function updateWidths() {
+//   let widths = {
+//     fourg: { dom: N4G, value: 0 },
+//     gsm: { dom: gsm, value: 0 },
+//     lte: { dom: lte, value: 0 },
+//     fiveg: { dom: fiveg, value: 0 },
+//     rf: { dom: rf, value: 0 },
+//   };
+
+//   setInterval(() => {
+//     if (widths["fourg"].value < 100) {
+//       widths["fourg"].value += 100 / 1;
+//       N4G.style.width = widths["fourg"].value + "%";
+//     }
+//     if (widths["gsm"].value < 100) {
+//       widths["gsm"].value += 100 / 2;
+//       gsm.style.width = widths["gsm"].value + "%";
+//     }
+//     if (widths["lte"].value < 100) {
+//       widths["lte"].value += 100 / 4;
+//       lte.style.width = widths["lte"].value + "%";
+//     }
+//     if (widths["fiveg"].value < 100) {
+//       widths["fiveg"].value += 100 / 6;
+//       fiveg.style.width = widths["fiveg"].value + "%";
+//     }
+//     if (widths["rf"].value < 100) {
+//       widths["rf"].value += 100 / 8;
+//       rf.style.width = widths["rf"].value + "%";
+//     }
+//   }, 1000);
+// }
+
 pulse.addEventListener("click", () => {
-  setInterval(() => {
-    two1();
-  }, 2000);
+  gsm.style.width = "100%";
+  update(N4G, 15);
+  update(lte, 35);
+  update(fiveg, 45);
+  update(rf, 85);
 });
